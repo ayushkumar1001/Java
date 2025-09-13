fe6df43ea83f1e25ef991dfda96ab6e1ebe36f8f
Ayush Kumar <ayushkumaraecit@gmail.com>
2025-09-13 20:55:46 +0530

    write my first java code

---
+++ 13-09-2025/First.java
@@ -0,0 +1,17 @@
+import java.lang.*;
+import java.util.*;
+class First {
+     public static void main(String args[])
+     {
+        String name;
+
+        Scanner sc=new Scanner(System.in);
+
+        System.out.println("May I Know Your Name");
+        name=sc.nextLine();
+
+        System.out.println("Welcome "+name);
+     }
+}
+
+
