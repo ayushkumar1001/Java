9981333b3dc48f61bf2e0accbd46bdb596e591f8
Ayush Kumar <ayushkumaraecit@gmail.com>
2025-09-18 21:04:18 +0530

    write a programme in bitwiseoperator, widenarrowing and some practice of quiz  and some modefication in previous code

---
+++ 18-09-2025/BitwiseOperator.java
@@ -0,0 +1,29 @@
+
+public class BitwiseOperator {
+
+    public static void main(String args[])
+    {
+       // int x=10,y=6,z;
+       int x=0b1010;
+       //int y=0b0110;
+       //int z;
+
+        //z=x&y;
+       //z=x|y;
+       //z=x^y;
+
+      // int x=0b1;
+      //int x=-0b1010;
+      int y;
+      //y=x<<2;
+    //  y=x>>1;
+      // y=x>>>1;
+      y=~x;
+
+        //System.out.println(z);
+          System.out.println(String.format("%s",Integer.toBinaryString(x)));
+         //System.out.println(String.format("%s",Integer.toBinaryString(y)));
+          System.out.println(String.format("%32s",Integer.toBinaryString(y)));
+          System.out.println(y);
+    }
+}
---
+++ 18-09-2025/WideNarrowing.java
@@ -0,0 +1,20 @@
+public class WideNarrowing {
+    public static void main(String args[])
+    {
+        byte b=5;
+        short s=120;
+        int i=10;
+        long l=10;
+
+        float f=10.5f;
+
+        double d=10;
+        char c=10;
+        
+        i=(int)f;
+
+        System.out.println(i);
+
+        
+    }
+}
