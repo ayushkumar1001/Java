6c0ec8f09d1da2a08898c7f1a0d873853d706bf6
Ayush Kumar <ayushkumaraecit@gmail.com>
2025-09-23 02:22:10 +0530

    learn about string to how to print in different condition

--- 19-09-2025/StringP.java
+++ 19-09-2025/StringP.java
@@ -1,8 +1,69 @@
 public class StringP {
     
-    public static void main(String args[]){
-        String str=new String("netbeans");
+    public static void main(String args[])
+    {
 
-         System.out.println(str.length());
+        //String str=new String("netbeans");
+
+        // System.out.println(str.length());
+        // int len=str.length();
+        // System.out.println(len);
+
+       // str.toUpperCase();
+        // System.out.println(str);
+
+    //    String str2=str.toUpperCase();
+    //     System.out.println(str2);
+
+        // str=str.toUpperCase();
+        // System.out.println(str);
+
+        // String str=new String("   netbeans   ");
+
+        // System.out.println(str);
+
+        // str=str.trim();
+
+        // System.out.println(str);
+
+        //String str=new String("netbeans");
+
+        // String str2=str.substring(3);
+        //   String str2=str.substring(3,7); // bean
+        //     String str2=str.substring(3,5); // -> be
+           // System.out.println(str2); // -> beans
+
+        // String str2=str.replace('e', 'M');
+
+        // System.out.println(str2+ " "+str); // -> nMtbMans netbeans
+
+      //  String str1="Mr. Shahrukh Khan";
+       // for(int i=0;i<str1.length();i++)
+         // System.out.println(str1.charAt(i)); // ->  show full name in multiple line 
+            //System.out.print(str1.charAt(i)); // ->  show fullname in one line
+
+        // System.out.println(str1.startsWith("Mr.")); // ->  True
+         //  System.out.println(str1.startsWith("mr.")); // ->  False
+           // System.out.println(str1.startsWith("Shah", 4)); // - > True
+            //System.out.println(str1.endsWith("Khan")); // -> True
+             // System.out.println(str1.charAt(4)); // - > S
+
+             String str1="www.udemy.co.in";
+
+             //System.out.println(str1.indexOf('.')); //start from index 3
+  
+             // System.out.println(str1.indexOf("udemy")); // start from index 4
+             //  System.out.println(str1.indexOf(".",4)); // start from index 9
+               // System.out.println(str1.lastIndexOf(".")); // start from index 12
+
+                 System.out.println(str1.indexOf("in")); // start from index 13
+          
+
+
+
+           
+
+     
+
     }
 }
---
+++ 20-09-2025/StringPractice.java
@@ -0,0 +1,113 @@
+public class StringPractice {
+
+    public static void main(String args[])
+    {
+        // String str1="Pyramid";
+
+        // String str2="Pyramid";
+
+        // System.out.println(str1.equals(str2)); // give result -> true
+
+
+        //  String str1="Pyramid";
+
+        // String str2="pyramid";
+
+       // System.out.println(str1.equals(str2)); // give result -> false
+
+
+        // String str1="Pyramid";
+
+        // String str2="Pyramid";
+
+        // System.out.println(str1.equalsIgnoreCase(str2)); // give result -> true
+
+
+        //     String str1="Pyramid"; // thgis will be store in string pool
+
+        //    String str2=new String("Pyramid"); // this will be store in heap
+
+        //    System.out.println(str1==str2);    // but the result will be false
+
+
+        //  String str1="Pyramid";
+
+        // String str2=new String("Pyramid");
+
+        // System.out.println(str1.equals(str2)); // the result will be true
+
+        
+        //  String str1="Pyramid";
+
+        //  String str2=new String("Pyramid");
+
+        //  System.out.println(str1.equals(str2)); //  result will be ->  true
+
+
+      //   str1==str2  -> then it will check the references , means addresses of the objects that are different.
+      //   str1.equals(str2) -> it will the contents of the string
+        
+       
+        //  String str1="Pyramid";
+
+        //  String str2=new String("Pyramid");
+
+        //  System.out.println(str1.compareTo(str2)); // result will be -> 0
+
+
+          // String str1="pyramid"; // this lowe case 
+                                                // lower case  p is greater and uppercase P is smaller because the ASCII codes are different.
+         // String str2=new String("Pyramid"); // this is uppercase
+
+         // System.out.println(str1.compareTo(str2)); // result will be -> 32
+
+          
+        //  String str1="pyramid";
+
+        //  String str2=new String("china wall");
+
+        //  System.out.println(str1.compareTo(str2)); // result will be -> 13
+
+         
+        //  String str1="china wall";
+
+        //  String str2=new String("pyramid");
+
+        //  System.out.println(str1.compareTo(str2));  // the result will be -> -13
+
+
+        //  String str1="china wall";
+
+        //  String str2=new String("china tall");
+
+        //  System.out.println(str1.compareTo(str2));  // result will be -> 3
+
+        
+        //  String str1="china wall";
+
+        //  String str2=new String("china all");
+
+        //  System.out.println(str1.compareTo(str2));  // result will be -> 22
+
+        
+        //  String str1="the great wall of china";
+
+        //  System.out.println(str1.contains("wall"));  // result will be -> true
+
+         
+        //  String str1="the great wall";
+
+        //  String str2=" of china";
+
+        //  System.out.println(str1.concat(str2)); // result will be -> the great wall of china
+        //                                         // concatenation of two strings
+        //  System.out.println(str1 + str2); // result will be -> the great wall of china
+
+        
+
+
+ 
+        
+    }
+    
+}
