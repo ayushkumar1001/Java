16550e8281f82a82c19b48339d3f8e27dbbf3086
Ayush Kumar <ayushkumaraecit@gmail.com>
2025-09-30 16:19:29 +0530

    learn about AP ,GP,FibonnaciSeries and Nested Loops and some Pattern question

---
+++ 30-09-2025/Pattern6Loops.java
@@ -0,0 +1,15 @@
+public class Pattern6Loops {
+
+    public static void main(String argsp[])
+    {
+        for(int i=1;i<=5;i++)
+        {
+            for(int j=1;j<=i;j++)
+            {
+                System.out.print("* ");
+            }
+            System.out.println("");
+        }
+    }
+    
+}
---
+++ 30-09-2025/Pattern7Loops.java
@@ -0,0 +1,20 @@
+public class Pattern7Loops {
+
+    public static void main(String args[])
+    {
+        int count=0;
+        for(int i=1;i<=5;i++)
+        {
+            for(int j=1;j<=5-i+1;j++)
+            {
+                // System.out.print(j+" ");
+                // System.out.print(i+" ");
+                count++;
+                // System.out.print(count+" ");
+                 System.out.format("%02d ",count);
+            }
+            System.out.println("");
+        }
+    }
+    
+}
