c0725db5b311e5966c54a919e533f54519c37c2d
Ayush Kumar <ayushkumaraecit@gmail.com>
2025-11-02 23:55:23 +0530

    Practice some conditional statement question

---
+++ Practice/Operators.java
@@ -0,0 +1,22 @@
+public class Operators {
+
+    public static void main(String args[])
+    {
+        int a=10, b=5;
+
+        //Addition
+        System.out.println("a + b = " + (a+b));
+
+        //substraction
+        System.out.println("a  - b = " + (a-b));
+
+        //multiplaction
+        System.out.println("a * b =" + (a*b));
+
+        //Division
+        System.out.println("a / b = " + (a / b));
+
+        //Modulus
+        System.out.println("a % b = " + (a%b));
+    }
+}
